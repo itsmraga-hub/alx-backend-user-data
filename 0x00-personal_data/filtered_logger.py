29e2249007faf6d770c9d4aefbf183bab27b9470
itsmraga-hub <ragawilliam570@gmail.com>
2023-08-31 16:47:59 +0300

    Task 1: Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,26 @@
 """
 
 import re
+import logging
+import re
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_message = super().format(record)
+        return re.sub(r'(' + '|'.join(self.fields) +
+                      r')=[^{}]*'.format(self.SEPARATOR),
+                      r'\1=' + self.REDACTION, log_message)
 
 
 def filter_datum(fields, redaction, message, separator):
