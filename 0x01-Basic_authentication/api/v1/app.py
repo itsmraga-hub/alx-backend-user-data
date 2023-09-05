fa7d98671000e39d22aa681c3abc614ed5d6503c
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-05 22:24:29 +0300

    Task 1: Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -14,11 +14,23 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
+    """
+        Not found handler
     """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+        Not found handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
 
 
 if __name__ == "__main__":
