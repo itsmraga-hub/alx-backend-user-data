87afcb484a71146767a59fc205590660260b863d
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-06 02:56:33 +0300

    Task 4: define which routes don't need authentication

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -17,7 +17,16 @@ class Auth:
             returns False - path and excluded_paths will be used later,
             now, you don’t need to take care of them
         """
-        return False
+
+        if path is None or excluded_paths is None or not len(excluded_paths):
+            return True
+        if path[-1] != '/':
+            path += '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
