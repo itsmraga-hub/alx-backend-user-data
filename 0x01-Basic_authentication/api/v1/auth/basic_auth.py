2a1c3cf8ab3673b42ee9f521c3f3526ad500b41e
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-06 19:00:00 +0300

    Task 6: Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    class BasicAuth that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+        class BasicAuth that inherits from Auth
+    """
+    pass
