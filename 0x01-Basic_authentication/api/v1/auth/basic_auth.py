389dcfd12a923f3d9a49f241e0af57d3cc4bee83
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-08 04:00:01 +0300

    Task 11: Basic Overload current user and BOOM

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,7 +4,8 @@
 """
 from api.v1.auth.auth import Auth
 from base64 import b64decode, decode
-from typing import Tuple
+from typing import Tuple, TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -67,3 +68,40 @@ class BasicAuth(Auth):
 
         name_p = decoded_base64_authorization_header.split(':')
         return tuple(name_p)
+
+    def user_object_from_credentials(self, user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        """
+            returns the User instance based on his email and password.
+        """
+        if user_email is None or not isinstance(user_email, str):
+            return None
+
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        try:
+            users = User.search(email=user_email)
+            for user in users:
+                if user.is_valid_password(user_pwd):
+                    return user
+
+        except Exception:
+            return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+            class BasicAuth that overloads Auth and retrieves the User
+            instance for a request:
+        """
+        auth_header = self.authorization_header(request)
+        if not auth_header:
+            return None
+        extract_base64 = self.extract_base64_authorization_header(auth_header)
+        decode_base64 = self.decode_base64_authorization_header(extract_base64)
+        user_credentials = self.extract_user_credentials(decode_base64)
+        user_email = user_credentials[0]
+        user_password = user_credentials[1]
+        user_credentials = self.user_object_from_credentials(
+            user_email, user_password)
+        return user_credentials
