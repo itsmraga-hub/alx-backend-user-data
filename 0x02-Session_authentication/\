926831ec15003ce8f3f7fda4a648251241d18c0a
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-09 07:53:21 +0300

    Task 1: Empty session

---
+++ 0x02-Session_authentication/\
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+    Create a class SessionAuth that inherits from Auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+        Create a class SessionAuth that inherits from Auth
+    """
+    pass
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -31,8 +31,9 @@ def view_one_user(user_id: str = None) -> str:
         if request.current_user is None:
             abort(404)
         else:
-            user = User.get(user_id)
-            return jsonify(user.to_json())
+            return jsonify(request.current_user.to_json())
+
+    user = User.get(user_id)
     if user is None:
         abort(404)
     return jsonify(user.to_json())
