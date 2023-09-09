3b2df67d9c14d8133b7c35f2af020720d6f6bfb2
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-09 08:19:53 +0300

    Fix

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,19 +1,27 @@
 #!/usr/bin/env python3
 """
     Create a class SessionAuth that inherits from Auth
+    instance for the variable auth depending of the value of the
+    environment variable AUTH_TYPE, If AUTH_TYPE is equal to session_auth:
 """
+
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     """
         Create a class SessionAuth that inherits from Auth
+        instance for the variable auth depending of the value of the
+        environment variable AUTH_TYPE, If AUTH_TYPE is equal to
+        session_auth:
     """
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str str = None) -> str:
         """
             creates a Session ID for a user_id:
+            Return None if user_id is None
+            Return None if user_id is not a string
         """
         if user_id is None or not isinstance(user_id, str):
             return None
