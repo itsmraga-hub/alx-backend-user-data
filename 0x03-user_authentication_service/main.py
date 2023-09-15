a26578accfec4552878f19675c2ef3c12e97fc4f
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-15 13:28:30 +0300

    Task 15: User profile

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -33,3 +33,15 @@ print(auth.valid_login(email, password))
 print(auth.valid_login(email, "WrongPwd"))
 
 print(auth.valid_login("unknown@email", password))
+
+
+from auth import Auth
+
+email = 'bob@bob.com'
+password = 'MyPwdOfBob'
+auth = Auth()
+
+auth.register_user(email, password)
+
+print(auth.create_session(email))
+print(auth.create_session("unknown@email.com"))
