2ce914fe8ccfdffdf769112cc8f2daf2221a80d0
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-15 12:27:42 +0300

    Task 7: create a user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,10 +3,12 @@
     In this task, you will set up a basic Flask app.
 """
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, abort, request, redirect
+from auth import Auth
 
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=['GET'])
@@ -15,6 +17,26 @@ def root() -> str:
         root page to respond with jsonified message
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def register_user() -> str:
+    """
+        Define a users function that implements the POST /users route.
+    """
+    try:
+        email = request.form['email']
+        password = request.form['password']
+    except KeyError:
+        abort(400)
+
+    try:
+        user = AUTH.register_user(email, password)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+
+    msg = {"email": email, "message": "user created"}
+    return jsonify(msg)
 
 
 if __name__ == "__main__":
