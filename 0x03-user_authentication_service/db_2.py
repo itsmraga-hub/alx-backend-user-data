e18ef416d0bf8d8227c82abd1197baa1b0f5526c
itsmraga-hub <ragawilliam570@gmail.com>
2023-09-15 12:00:58 +0300

    Fix

--- 0x03-user_authentication_service/db_2.py
+++ 0x03-user_authentication_service/db_2.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3
-""" Database for ORM """
+"""DB module
+"""
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from typing import TypeVar
+
 from user import Base, User
 
 
 class DB:
-    """ DB Class for Object Reational Mapping """
+    """DB class
+    """
 
-    def __init__(self):
-        """ Constructor Method """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+    def __init__(self) -> None:
+        """Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
-    def _session(self):
-        """ Session Getter Method """
+    def _session(self) -> Session:
+        """Memoized session object
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """ Adds user to database
-        Return: User Object
+        """
+            Implement the add_user method, which has two required string
+            arguments: email and hashed_password, and returns a User object
         """
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
@@ -38,15 +45,15 @@ class DB:
         return user
 
     def find_user_by(self, **kwargs) -> User:
-        """ Finds user by key word args
-        Return: First row found in the users table as filtered by kwargs
+        """
+             implement the DB.find_user_by method
         """
         if not kwargs:
             raise InvalidRequestError
 
-        column_names = User.__table__.columns.keys()
+        columns = User.__table__.columns.keys()
         for key in kwargs.keys():
-            if key not in column_names:
+            if key not in columns:
                 raise InvalidRequestError
 
         user = self._session.query(User).filter_by(**kwargs).first()
@@ -57,14 +64,13 @@ class DB:
         return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
-        """ Update users attributes
-        Returns: None
+        """
+            
         """
         user = self.find_user_by(id=user_id)
-
-        column_names = User.__table__.columns.keys()
+        columns = User.__table__.columns.keys()
         for key in kwargs.keys():
-            if key not in column_names:
+            if key not in columns:
                 raise ValueError
 
         for key, value in kwargs.items():
